7c065eadada8c34ff86dfadd6c88a74c4a47332d
Neet Shah <shahn17@mcmaster.ca>
2018-10-12 01:26:33 -0400

    Added HTTP request code

--- app/src/main/java/com/example/neetshah/cryptocharts/MainActivity.java
+++ app/src/main/java/com/example/neetshah/cryptocharts/MainActivity.java
@@ -2,6 +2,21 @@ package com.example.neetshah.cryptocharts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
+        getData();
+    }
+
+    public void getData() {
+        RequestData a = new RequestData();
+        a.execute();
+//        a.doInBackground(null);
+//        return null;
     }
 }
