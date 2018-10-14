1cdb8c67bed98a0d6c55d5c0dc79e9520e837900
Neet Shah <shahn17@mcmaster.ca>
2018-10-14 02:56:51 -0400

    Creates models of all the data from api

--- app/src/main/java/com/example/neetshah/cryptocharts/RequestData.java
+++ app/src/main/java/com/example/neetshah/cryptocharts/RequestData.java
@@ -14,16 +14,60 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class RequestData extends AsyncTask {
 
+    private Double getPrice(String curr) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        try {
+            URL url = new URL("https://min-api.cryptocompare.com/data/price?fsym=" + URLEncoder.encode(curr, "UTF-8") + "&tsyms=CAD");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            InputStream stream = connection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+
+            line = reader.readLine();
+//            Log.d("Response: ", "> " + line);
+            JSONObject mainObject = new JSONObject(line);
+            String val = mainObject.getString("CAD");
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return 1.0;
+    }
+
     @Override
     protected Object doInBackground(Object[] objects) {
         HttpURLConnection connection = null;
         BufferedReader reader = null;
-
+        ArrayList<CurrencyData> currencyData = new ArrayList<>();
         try {
             URL url = new URL("https://www.cryptocompare.com/api/data/coinlist/");
             connection = (HttpURLConnection) url.openConnection();
@@ -38,56 +82,29 @@ public class RequestData extends AsyncTask {
 
             while ((line = reader.readLine()) != null) {
                 buffer.append(line + "\n");
-                Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+//                Log.d("Response: ", "> " + line);
                 JSONObject mainObject = new JSONObject(line);
                 JSONObject data = mainObject.getJSONObject("Data");
                 Iterator<String> iterator = data.keys();
                 ArrayList<String> names = new ArrayList<>();
-                while(iterator.hasNext()) {
+                while (iterator.hasNext()) {
                     String key = iterator.next();
+                    Double price = getPrice(key);
+                    if (price == null) {
+                        continue;
+                    }
                     JSONObject currency = (JSONObject) data.get(key);
                     String name = currency.getString("CoinName");
                     names.add(name);
+//                    Log.d("TAG", "NAME: " + key);
+                    CurrencyData currencyData1 = CurrencyData.builder()
+                            .fullName(name)
+                            .name(key)
+                            .isFavourite(false)
+                            .price(price)
+                            .build();
                 }
-                for(String name: names) {
-                    Log.d("Names: ", name);
-                }
-//                JSONObject value1 = (JSONObject)data.get("DIVX");
-//                Log.d("TAGmmm", value1.toString());
-//                String a = value1.getString("Name");
-////                JSONObject value2 = (JSONObject)uniObject.get("Name");
-//                Log.d("NAME: ", a.toString());
-
-//                Iterator<String> iter = uniObject.keys();
-//                while (iter.hasNext()) {
-//                    String key = iter.next();
-//                    try {
-//                        Object value = uniObject.get(key);
-//                        JSONObject b = (JSONObject) value;
-//                        Iterator<String> iter1 = b.keys();
-//                        String key1 =
-//                        Object c = b.get("Url");
-//                        JSONObject d = (JSONObject) c;
-//                        Log.d("TAG", d.toString());
-//                    } catch (JSONException e) {
-//                        // Something went wrong!
-//                    }
-//                }
-
-
-//                JSONObject object2 = uniObject.getJSONObject("365");
-//                JSONArray object3 = mainObject.getJSONArray("Data");
-//                Log.d("LENGHT: ", String.valueOf(object3.length()));
-//                for (int i =0; i<object3.length(); i++) {
-////                    Log.d("CHECK"+i, object3.getJSONObject(i).toString());
-//                    object3.getJSONObject(i);
-//                }
-//                JSONObject a = object3.getJSONObject(4);
-//                Log.d("TAG")
-//                Log.d("CHECK", array.toString());
             }
-
-            //return buffer.toString();
 
 
         } catch (MalformedURLException e) {
@@ -108,6 +125,7 @@ public class RequestData extends AsyncTask {
                 e.printStackTrace();
             }
         }
+        Log.d("TAG, ", "DONE");
         return null;
     }
 }
